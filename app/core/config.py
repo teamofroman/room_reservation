96b63d3e285b89920c8246fc542ffde622bdfcb1
Роман Александров <rsaleksandrov@yandex.ru>
2023-09-16 22:44:20 +0300

    Подготовка к созданию БД и таблицы

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     app_description: str = 'Все на переговоры!'
     app_version: str = '1.0.0'
+    database_url: str
 
     class Config:
         env_file = '.env'
