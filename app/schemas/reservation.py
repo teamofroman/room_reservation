9ff2a58ec9e397fa07491ede4bfe588be987a3ba
Александров Роман <rsateam@yandex.ru>
2023-09-21 08:46:07 +0300

    Добавили схемы для модели Reservation

---
+++ app/schemas/reservation.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from pydantic import BaseModel, validator, root_validator
+
+
+class SchemaReservationBase(BaseModel):
+    from_reserve: datetime
+    to_reserve: datetime
+
+
+class SchemaReservationDB(SchemaReservationBase):
+    id: int
+    meetingroom_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SchemaReservationUpdate(SchemaReservationBase):
+    @validator('from_reverse')
+    def check_from_reserve_later_than_now(cls, value: datetime):
+        if value <= datetime.now():
+            raise ValueError('Время начала не может быть раньше текущего '
+                             'времени')
+        return value
+
+    @root_validator(skip_on_failure=True)
+    def check_from_reserve_before_to_reserve(cls, values):
+        if values['to_reserve'] <= values['from_reserve']:
+            raise ValueError('Время окончания должно быть больше времени '
+                             'начала')
+        return values
+
+
+class SchemaReservationCreate(SchemaReservationUpdate):
+    meetingroom_id: int
